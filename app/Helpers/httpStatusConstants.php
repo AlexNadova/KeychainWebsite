06ffc57039c83cf9632f30f311557342b8d89794
Alexandra Nadova <alexandranadova@gmail.com>
2019-12-27 14:14:24 +0100

    37: added neccessary files

---
+++ app/Helpers/httpStatusConstants.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers;
+
+class HttpStatus{
+	const STATUS_OK = 200;
+	const STATUS_CREATED = 201;
+	const STATUS_BAD_REQUEST = 400;
+	const STATUS_UNAUTHORIZED = 401;
+	const STATUS_FORBIDDEN = 403;
+	const STATUS_UNPROCESSABLE_ENTITY = 422;
+	const STATUS_INTERNAL_SERVER_ERROR = 500;
+	const STATUS_CONFLICT = 409;
+}
